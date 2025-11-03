867a3b58d058718f7b867c20166b561dcbce3dbe
Lyon <t41589@gmail.com>
2025-11-03 15:12:48 -0500

    Add dark mode support, update dependencies, and configure path aliases

--- koinvote/src/main.tsx
+++ koinvote/src/main.tsx
@@ -1,7 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './global.css'
 import App from './App.tsx'
+import { Suspense } from 'react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Loading } from '@/components/Loading.tsx'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <Suspense fallback={<Loading />}>
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+    </Suspense>
 )
