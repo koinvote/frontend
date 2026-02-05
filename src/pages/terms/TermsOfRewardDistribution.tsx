a1580edfe2e8ef1629aab633b28dfe78361a8303
Winter Weng <yichinweng@gmail.com>
2026-02-05 17:57:17 +0800

    fix: [terms of reward] 修改範例數字

--- src/pages/terms/TermsOfRewardDistribution.tsx
+++ src/pages/terms/TermsOfRewardDistribution.tsx
@@ -29,7 +29,7 @@ export default function TermsOfRewardDistribution() {
       </h1>
 
       <p className="text-secondary leading-relaxed">
-        <Trans i18nKey="rewardTerms.intro" components={{ bold }} />
+        {t("rewardTerms.intro")}
       </p>
 
       <section>
@@ -152,96 +152,31 @@ export default function TermsOfRewardDistribution() {
       <section>
         <h2 className="text-xl fw-m mb-2">{t("rewardTerms.examplesTitle")}</h2>
         <p className="text-secondary mb-2">
-          <Trans i18nKey="rewardTerms.examplesIntro" components={{ bold }} />
+          {t("rewardTerms.examplesIntro")}
         </p>
 
         <div className="space-y-3">
           <div className="space-y-1">
             <h3 className="font-bold">{t("rewardTerms.ex1_title")}</h3>
             <div className="space-y-1 text-secondary">
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex1_total_reward"
-                  components={{ bold }}
-                />
-              </p>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex1_winners"
-                  components={{ bold }}
-                />
-              </p>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex1_participants_label"
-                  components={{ bold }}
-                />
-              </p>
+              <p>{t("rewardTerms.ex1_total_reward")}</p>
+              <p>{t("rewardTerms.ex1_winners")}</p>
+              <p>{t("rewardTerms.ex1_participants_label")}</p>
               <ul className="list-disc pl-6 space-y-1">
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_participant_a"
-                    components={{ bold }}
-                  />
-                </li>
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_participant_b"
-                    components={{ bold }}
-                  />
-                </li>
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_participant_c"
-                    components={{ bold }}
-                  />
-                </li>
+                <li>{t("rewardTerms.ex1_participant_a")}</li>
+                <li>{t("rewardTerms.ex1_participant_b")}</li>
+                <li>{t("rewardTerms.ex1_participant_c")}</li>
               </ul>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex1_selection_label"
-                  components={{ bold }}
-                />
-              </p>
+              <p>{t("rewardTerms.ex1_selection_label")}</p>
               <ul className="list-disc pl-6 space-y-1">
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_selection_1"
-                    components={{ bold }}
-                  />
-                </li>
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_selection_2"
-                    components={{ bold }}
-                  />
-                </li>
+                <li>{t("rewardTerms.ex1_selection_1")}</li>
+                <li>{t("rewardTerms.ex1_selection_2")}</li>
               </ul>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex1_distribution_label"
-                  components={{ bold }}
-                />
-              </p>
+              <p>{t("rewardTerms.ex1_distribution_label")}</p>
               <div className="space-y-1">
-                <p>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_distribution_1"
-                    components={{ bold }}
-                  />
-                </p>
-                <p>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_distribution_2"
-                    components={{ bold }}
-                  />
-                </p>
-                <p>
-                  <Trans
-                    i18nKey="rewardTerms.ex1_distribution_3"
-                    components={{ bold }}
-                  />
-                </p>
+                <p>{t("rewardTerms.ex1_distribution_1")}</p>
+                <p>{t("rewardTerms.ex1_distribution_2")}</p>
+                <p>{t("rewardTerms.ex1_distribution_3")}</p>
               </div>
             </div>
           </div>
@@ -249,76 +184,37 @@ export default function TermsOfRewardDistribution() {
           <div className="space-y-1">
             <h3 className="font-bold">{t("rewardTerms.ex2_title")}</h3>
             <div className="space-y-1 text-secondary">
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex2_total_reward"
-                  components={{ bold }}
-                />
-              </p>
+              <p>{t("rewardTerms.ex2_total_reward")}</p>
               <p>
                 <Trans
                   i18nKey="rewardTerms.ex2_platform_fee"
                   components={{ bold }}
                 />
               </p>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex2_transaction_fee"
-                  components={{ bold }}
-                />
-              </p>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex2_min_threshold"
-                  components={{ bold }}
-                />
-              </p>
+              <p>{t("rewardTerms.ex2_transaction_fee")}</p>
+              <p>{t("rewardTerms.ex2_min_threshold")}</p>
               <p>
                 <Trans
                   i18nKey="rewardTerms.ex2_net_reward"
                   components={{ bold }}
                 />
               </p>
-              <p>
-                <Trans
-                  i18nKey="rewardTerms.ex2_winners_label"
-                  components={{ bold }}
-                />
-              </p>
+              <p>{t("rewardTerms.ex2_winners_label")}</p>
               <ul className="list-disc pl-6 space-y-1">
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex2_winner_a"
-                    components={{ bold }}
-                  />
-                </li>
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex2_winner_b"
-                    components={{ bold }}
-                  />
-                </li>
-                <li>
-                  <Trans
-                    i18nKey="rewardTerms.ex2_winner_c"
-                    components={{ bold }}
-                  />
-                </li>
+                <li>{t("rewardTerms.ex2_winner_a")}</li>
+                <li>{t("rewardTerms.ex2_winner_b")}</li>
+                <li>{t("rewardTerms.ex2_winner_c")}</li>
               </ul>
               <p>
                 <Trans
-                  i18nKey="rewardTerms.ex2_steps_label"
+                  i18nKey="rewardTerms.ex2_total_holdings"
                   components={{ bold }}
                 />
               </p>
+              <p>{t("rewardTerms.ex2_steps_label")}</p>
               <ol className="list-decimal pl-6 marker:font-bold space-y-1">
                 <li className="space-y-1">
-                  <p className="font-bold">
-                    <Trans
-                      i18nKey="rewardTerms.ex2_step1_label"
-                      components={{ bold }}
-                    />
-                  </p>
+                  <p className="font-bold">{t("rewardTerms.ex2_step1_label")}</p>
                   <ul className="list-disc pl-6 space-y-1">
                     <li>
                       <Trans
@@ -347,62 +243,22 @@ export default function TermsOfRewardDistribution() {
                   </ul>
                 </li>
                 <li className="space-y-1">
-                  <p className="font-bold">
-                    <Trans
-                      i18nKey="rewardTerms.ex2_step2_label"
-                      components={{ bold }}
-                    />
-                  </p>
+                  <p className="font-bold">{t("rewardTerms.ex2_step2_label")}</p>
                   <ul className="list-disc pl-6 space-y-1">
-                    <li>
-                      <Trans
-                        i18nKey="rewardTerms.ex2_step2_a"
-                        components={{ bold }}
-                      />
-                    </li>
-                    <li>
-                      <Trans
-                        i18nKey="rewardTerms.ex2_step2_b"
-                        components={{ bold }}
-                      />
-                    </li>
-                    <li>
-                      <Trans
-                        i18nKey="rewardTerms.ex2_step2_remainder"
-                        components={{ bold }}
-                      />
-                    </li>
+                    <li>{t("rewardTerms.ex2_step2_a")}</li>
+                    <li>{t("rewardTerms.ex2_step2_b")}</li>
+                    <li>{t("rewardTerms.ex2_step2_remainder")}</li>
                   </ul>
                 </li>
                 <li className="space-y-1">
-                  <p className="font-bold">
-                    <Trans
-                      i18nKey="rewardTerms.ex2_step3_label"
-                      components={{ bold }}
-                    />
-                  </p>
+                  <p className="font-bold">{t("rewardTerms.ex2_step3_label")}</p>
                   <ul className="list-disc pl-6 space-y-1">
-                    <li>
-                      <Trans
-                        i18nKey="rewardTerms.ex2_step3_1"
-                        components={{ bold }}
-                      />
-                    </li>
-                    <li>
-                      <Trans
-                        i18nKey="rewardTerms.ex2_step3_2"
-                        components={{ bold }}
-                      />
-                    </li>
+                    <li>{t("rewardTerms.ex2_step3_1")}</li>
+                    <li>{t("rewardTerms.ex2_step3_2")}</li>
                   </ul>
                 </li>
                 <li className="space-y-1">
-                  <p className="font-bold">
-                    <Trans
-                      i18nKey="rewardTerms.ex2_final_label"
-                      components={{ bold }}
-                    />
-                  </p>
+                  <p className="font-bold">{t("rewardTerms.ex2_final_label")}</p>
                   <ul className="list-disc pl-6 space-y-1">
                     <li>
                       <Trans
@@ -416,12 +272,7 @@ export default function TermsOfRewardDistribution() {
                         components={{ bold }}
                       />
                     </li>
-                    <li>
-                      <Trans
-                        i18nKey="rewardTerms.ex2_final_c"
-                        components={{ bold }}
-                      />
-                    </li>
+                    <li>{t("rewardTerms.ex2_final_c")}</li>
                   </ul>
                 </li>
               </ol>
