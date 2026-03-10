49141f622c4b4f1c6b75248905f64c49be5dc47c
Winter Weng <yichinweng@gmail.com>
2026-03-10 13:09:58 +0800

    fix: [consts] 更改 consts.

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 const CONSTS = {
   ADMIN_ADDRESS: "1hBbQvxWT9AuvmR4C8RnTbvJLw7FkKstY",
   SUPPORT_EMAIL: "support@koinvote.com",
-  EXTEND_BUTTON_THRESHOLD_MINUTES: 44, // 低於幾分鐘可延長
+  EXTEND_BUTTON_THRESHOLD_MINUTES: 30, // 低於幾分鐘可延長
   DEPOSIT_EXTEND_TIME: 30, // 延長幾分鐘 (此處用於文案和 mock，prod 由後端決定延長時間)
 };
 
